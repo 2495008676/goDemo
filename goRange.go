85e1512af913e23ad18df94bfac876750e428bcf
cuihuaiyu <cuihuaiyu@shein.com>
2018-08-15 00:30:46 +0800

    博客静态页面

--- goRange.go
+++ goRange.go
@@ -8,11 +8,11 @@ func main() {
 	//这是我们使用range去求一个slice的和。使用数组跟这个很类似
 	nums := []int{2, 3, 4}
 	sum := 0
-	for i, num := range nums {
+	for _, num := range nums {
         sum += num
     }
 	
-	fmt.Println('sum:',sum)
+	fmt.Println("sum:",sum)
 	
 	//在数组上使用range将传入index和值两个变量。上面那个例子我们不需要使用该元素的序号，所以我们使用空白符"_"省略了。有时侯我们确实需要知道它的索引。
 	for i,num := range nums {
--- src/blog/controllers/default.go
+++ src/blog/controllers/default.go
@@ -8,8 +8,50 @@ type MainController struct {
 	beego.Controller
 }
 
-func (c *MainController) Get() {
+ //index.html  首页
+func (c *MainController) Index() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
+	//c.TplName = "index.tpl"
+	c.TplName = "index.html"
+}
+
+//about.html  关于我
+func (c *MainController) About() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	//c.TplName = "index.tpl"
+	c.TplName = "about.html"
+}
+
+//article.html 学无止境（文章列表）
+func (c *MainController) Article() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	//c.TplName = "index.tpl"
+	c.TplName = "article.html"
+}
+
+//article_detail.html 文章详情
+func (c *MainController) ArticleDetail() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	//c.TplName = "index.tpl"
+	c.TplName = "article_detail.html"
+}
+
+//board.html 留言板
+func (c *MainController) Board() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	//c.TplName = "index.tpl"
+	c.TplName = "board.html"
+}
+
+//mood.html  碎言碎语
+func (c *MainController) Mood() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	//c.TplName = "index.tpl"
+	c.TplName = "mood.html"
 }
